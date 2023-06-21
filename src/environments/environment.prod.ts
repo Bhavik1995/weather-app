b69a33ac60a74b9090b7982e24d9dd0d11d3066d
bhavik.sejpal <bhavik.sejpal@knoxpo.com>
2023-06-21 13:54:50 +0530

    update env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city',
+  XRapidAPIHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHeaderValue: 'open-weather13.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '5063d3a949msh3afd2f0c31377e6p1f8be4jsn1608d0397638'
 };
