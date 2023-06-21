81ab80069b06d7ce57f20ab58ce5bf8d7ac65d8d
bhavik.sejpal <bhavik.sejpal@knoxpo.com>
2023-06-21 13:38:43 +0530

    weather app completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'weather-app';
+export class AppComponent implements OnInit {
+
+  constructor(private weatherService: WeatherService) {
+
+  }
+
+  weatherData?: WeatherData;
+  cityName: string = 'Mumbai';
+  temp_city: any;
+  temp_city_celsius: any;
+  temp_max_celsius: any;
+  temp_min_celsius: any;
+
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  onSubmit(){
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  private getWeatherData(cityName: string){
+
+    this.weatherService.getWeatherData(cityName)
+      .subscribe({
+        next: (res) => {
+          this.weatherData = res;
+
+          const fTempMain = res.main.temp;
+          this.temp_city_celsius = (fTempMain - 32) * 5 / 9;
+
+          const fTempMax = res.main.temp_max;
+          this.temp_max_celsius = (fTempMax - 32) * 5 / 9;
+
+          const temp_min_celsius = res.main.temp_min;
+          this.temp_min_celsius = (temp_min_celsius - 32) * 5 / 9;
+        }
+      })
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city',
+  XRapidAPIHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHeaderValue: 'open-weather13.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '5063d3a949msh3afd2f0c31377e6p1f8be4jsn1608d0397638'
 };
 
 /*
